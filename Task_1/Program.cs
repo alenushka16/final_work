f3701f877bbbe806226ac3aae31c2492f65b8aba
unknown <you@example.com>
2022-11-30 11:15:06 +0300

    change task

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,20 +1,36 @@
-﻿string[] CreateStringArray1(int n) {  
-    string[] array = new string[n]; 
-        for (int i = 0; i < n; i++) {
-            Console.Write("Введите элемент массива №{0}:\n", i + 1);
-            array[i] = Console.ReadLine();             
+﻿// string[] stringArray = {"hello", "2", "world",":-)"};
+// string[] stringArray = {"Russia", "Denmark", "Kazan"};
+// string[] stringArray = {"1234", "1567", "-2", "computer science"};
+
+string[] CreateUserStringArray(int length)
+{
+    string[] array = new string[length];
+
+    for(int i = 0; i < length; i++)
+    {
+        Console.Write("\nВведите элемент массива: ");
+        string? value = Console.ReadLine();
+        while(value == String.Empty)
+        {
+            Console.Write("\nВы не ввели значение. Введите элемент массива: ");
+            value = Console.ReadLine();
+        }
+        if(value != null) array[i] = value;
     }
     return array;
 }
-string[] CreateStringArray2(string[] array) {
-    int count = 0;
-    for(int i = 0; i < array.Length; i++) {
-        if(array[i].Length <= 3) 
-        count++;
-    }
-    string[] newArray = new string[count];
-    for(int i = 0, j = 0; i < array.Length; i++) {
-        if(array[i].Length <= 3) 
+
+string[] CreateStringArray(string[] array, int numberSymbol = 3)
+{
+    int lengthNewArray = 0;
+    for(int i = 0; i < array.Length;i++)
+        if(array[i].Length <= numberSymbol) lengthNewArray++;
+
+    string[] newArray = new string[lengthNewArray];
+
+    for(int i = 0, j = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= numberSymbol)
         {
             newArray[j] = array[i];
             j++;
@@ -22,24 +38,42 @@ string[] CreateStringArray2(string[] array) {
     }
     return newArray;
 }
-void PrintStringArray(string[] array) {
-    Console.Write("[");
-    for(int i = 0; i < array.Length; i++) {
-        Console.Write("\"" + array[i] + "\"");
-        if(i < array.Length -1)  
-            Console.Write(", ");
+
+void PrintStringArray(string[] array)
+{
+    Console.Write("\nСозданный массив: [");
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write("'" + array[i] + "'");
+        if(i < array.Length -1) Console.Write(",");
     }
     Console.Write("]");
-    Console.Write(" -> ");
+    Console.WriteLine();
 }
-Console.Write("Введите количество строк массива, которые хотите ввести:\n");
-int n = Convert.ToInt32(Console.ReadLine()); 
-string[] array = CreateStringArray1(n);
-PrintStringArray(array);
-string[] myArray = CreateStringArray2(array);
-PrintStringArray(myArray);
-Console.WriteLine();
-Console.WriteLine("Нажмите [Enter] для выхода...");
+
+try
+{
+    Console.Write("Введите размер массива: ");
+    int sizeArray = Convert.ToInt32(Console.ReadLine());
+    while(sizeArray < 0)
+    {
+        Console.WriteLine("\nРазмер массива - это целое положительное число.");
+        Console.Write("\nВведите новое значение: ");
+        sizeArray = Convert.ToInt32(Console.ReadLine());
+    }
+    if(sizeArray == 0) 
+        Console.WriteLine("\nРазмер массива = 0. Массив пустой: []");
+    else 
+    {
+        string[] stringArray = CreateUserStringArray(sizeArray);
+        PrintStringArray(stringArray);
+        string[] newStringArray = CreateStringArray(stringArray);
+        PrintStringArray(newStringArray);
+    }
+}
+catch(Exception)
+{
+    Console.WriteLine("\nНедопустимый ввод. Конвертация невозможна!");
+}
+
 Console.ReadLine();
-
-
